4358a76eddef6c2c2105d3363fb8b965dfaa848c
FelipeMDantas <felipemdantas@terra.com.br>
2022-09-08 21:58:56 -0300

    Feed, Sidebar and Videos components defined.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar, Videos } from "./";
 
-const Feed = () => {
-  return <div>Feed</div>;
-};
+const Feed = () => (
+  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Box
+      sx={{
+        height: { sx: "auto", md: "92vh" },
+        borderRight: "1px solid #3d3d3d",
+        px: { sx: 0, md: 2 },
+      }}
+    >
+      <Sidebar />
+      <Typography
+        className="copyright"
+        variant="body2"
+        sx={{ mt: 1.5, color: "#fff" }}
+      >
+        Copyright 2022 FelipeMDantas VideoSharing
+      </Typography>
+    </Box>
+
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        New <span style={{ color: "#f31503" }}>Videos</span>
+      </Typography>
+
+      <Videos />
+    </Box>
+  </Stack>
+);
 
 export default Feed;
