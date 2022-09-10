28cf850eccd5d72f512ce4021de0f63db2fb524f
FelipeMDantas <felipemdantas@terra.com.br>
2022-09-09 23:49:37 -0300

    Connection with RapidAPI configured.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,33 +1,53 @@
 import { Box, Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { Sidebar, Videos } from "./";
 
-const Feed = () => (
-  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-    <Box
-      sx={{
-        height: { sx: "auto", md: "92vh" },
-        borderRight: "1px solid #3d3d3d",
-        px: { sx: 0, md: 2 },
-      }}
-    >
-      <Sidebar />
-      <Typography
-        className="copyright"
-        variant="body2"
-        sx={{ mt: 1.5, color: "#fff" }}
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
       >
-        Copyright 2022 FelipeMDantas VideoSharing
-      </Typography>
-    </Box>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022 FelipeMDantas VideoSharing
+        </Typography>
+      </Box>
 
-    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        New <span style={{ color: "#f31503" }}>Videos</span>
-      </Typography>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#f31503" }}>Videos</span>
+        </Typography>
 
-      <Videos />
-    </Box>
-  </Stack>
-);
-
+        <Videos />
+      </Box>
+    </Stack>
+  );
+};
 export default Feed;
