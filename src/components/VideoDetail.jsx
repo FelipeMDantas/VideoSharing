013561fcd1b660d85648c76414c9acc89e9f69ac
FelipeMDantas <felipemdantas@terra.com.br>
2022-09-13 23:55:22 -0300

    Video rendering bug fixed.

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,18 +1,27 @@
+import { CheckCircle } from "@mui/icons-material";
 import { Box, Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import { Link, useParams } from "react-router-dom";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+import Videos from "./Videos";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
-  const { id } = useParams;
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
     );
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
+    );
   }, [id]);
+
+  if (!videoDetail?.snippet) return "Loading...";
 
   const {
     snippet: { title, channelId, channelTitle },
@@ -25,11 +34,13 @@ const VideoDetail = () => {
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
             <ReactPlayer
+              //className="react-pĺayer"
               url={`https://www.youtube.com/watch?v=${id}`}
-              className="react-pĺayer"
+              width="100%"
+              height="77vh"
               controls
             />
-            <Typography color="#fff" variant="h5" fontWeigth="bold" p={2}>
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
             </Typography>
             <Stack
@@ -40,12 +51,38 @@ const VideoDetail = () => {
               px={2}
             >
               <Link to={`/channel/${channelId}`}>
-                <Typography>{channelTitle}</Typography>
+                <Typography
+                  variant={{ sm: "subtitle1", md: "h6" }}
+                  color="#fff"
+                >
+                  {channelTitle}
+                  <CheckCircle
+                    sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+                  />
+                </Typography>
               </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
             </Stack>
           </Box>
         </Box>
       </Stack>
+
+      <Box
+        px={2}
+        py={{ md: 1, xs: 5 }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        {console.log(videos)}
+        {/* <Videos videos={videos} /> */}
+      </Box>
     </Box>
   );
 };
